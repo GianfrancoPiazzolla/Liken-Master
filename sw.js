92e5131bffe11079ef2186778e18b36c96814a60
Gianfranco Piazzolla <gianfranco.piazzolla@gmail.com>
2026-03-31 15:38:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 const CACHE_NAME = 'liken-master-cache-v1';
-const urlsToCache = [
+const ASSETS_TO_CACHE = [
   '/',
   'index.html',
+  'manifest.json',
   'favicon.ico',
   'icon-192x192.png',
   'icon-512x512.png'
@@ -9,23 +10,28 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
   );
 });
